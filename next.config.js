5a6b55f8614de19605284f60ce2b8457563239a4
AprilFoolsDay0401 <dkkim6@uci.edu>
2025-05-20 06:32:49 -0700

    final

--- next.config.js
+++ next.config.js
@@ -4,6 +4,32 @@ const nextConfig = {
     ignoreDuringBuilds: true,
     dirs: ["src"],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    domains: [
+      "pagead2.googlesyndication.com",
+      "googleads.g.doubleclick.net",
+      "www.google.com",
+      "www.googletagservices.com",
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["sharp"],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        "node:buffer": "commonjs node:buffer",
+        "node:async_hooks": "commonjs node:async_hooks",
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
