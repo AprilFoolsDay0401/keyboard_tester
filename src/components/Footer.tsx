4a5c0c9558be69fb37e84fa81de8e2dd9e8491d3
AprilFoolsDay0401 <dkkim6@uci.edu>
2025-05-20 07:17:17 -0700

    Change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="text-center text-white mt-6 w-full p-4">
-      <p className="mb-2">Made with ❤️ by DK</p>
+      <p className="mb-2">Made with ❤️ by KeyboardTest</p>
       <div className="w-60 h-0.5 bg-gradient-to-r from-transparent via-white to-transparent mx-auto mb-2"></div>
       <div className="flex justify-center space-x-4 text-sm">
         <Link href="/about" className="hover:text-blue-400 transition-colors">
