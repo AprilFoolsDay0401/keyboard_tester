e9d8fd00814ab62d6c7a0325a4757df3feb40447
AprilFoolsDay0401 <dkkim6@uci.edu>
2025-05-20 07:52:40 -0700

    Update the google ad info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1884792243517562"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
